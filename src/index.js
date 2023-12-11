505e6428c61ee75475c322e0a1c74695c35e5b08
Haki Celmeta <celmeta.haki@gmail.com>
2023-12-11 22:44:38 +0100

    Finished home and about

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './styles/main.scss'
+import css from './assets/css.png';
+import html5 from './assets/html5.png';
+import tailwind from './assets/tailwind.png';
+import java from './assets/java.png';
+import javascript from './assets/javascript.png';
+import mongodb from './assets/mongodb.png';
+import node_js from './assets/node_js.png';
+import npm from './assets/npm.png';
+import python from './assets/python.png';
+import sass from './assets/sass.png';
+import django from './assets/django.png';
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Battleship',			//Name of the website
+      filename: 'index.html',			//where html goes bundle
+      template: 'src/template.html', 	//html we write
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+}
