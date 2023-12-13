487850f23692152e33b45d5e7073a77a3f776537
Haki Celmeta <celmeta.haki@gmail.com>
2023-12-13 23:05:53 +0100

    Finished mobile design

--- src/index.js
+++ src/index.js
@@ -13,3 +13,40 @@ import django from './assets/django.png';
 import battleship from './assets/battleship.PNG';
 import weather from './assets/weather.PNG';
 import ageCalulator from './assets/ageCalulator.PNG';
+import jw from './assets/J.W.png';
+import hamburgerMenu from './assets/hamburger-menu-svgrepo-com.svg';
+
+const menu = document.querySelector('#hamburger-menu');
+const menuContainer = document.querySelector('.menu-container');
+
+document.querySelectorAll('.menu-container a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+let isOpen = false;
+
+menu.addEventListener('click', () => {
+  if (!isOpen) {
+    menuContainer.style.display = 'flex';
+    isOpen = true;
+  } else {
+    menuContainer.style.display = 'none';
+    isOpen = false;
+  }
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 500) {
+    menuContainer.style.display = 'flex';
+  }
+
+  if (window.innerWidth <= 500) {
+    menuContainer.style.display = 'none';
+  }
+})
